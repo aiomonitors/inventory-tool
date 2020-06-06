65123bc56d442b17420bd51f57b4fa80eb70ab08
Shihab Chowdhury <sachow168@gmail.com>
2020-06-06 00:22:43 -0400

    Added type for inventory item

--- src/common/types.d.ts
+++ src/common/types.d.ts
@@ -5,3 +5,15 @@ export interface ReleaseProduct {
     price: number | string;
     date?: string;
 }
+
+export interface InventoryItem {
+    name: string;
+    sku: string;
+    purchasePrice: number;
+    size: string;
+    marketPrice?: number;
+    image?: string;
+    category?: string;
+}
+
+export type Action = { label: string; value: string };
